bef29a8077995ef30c5537a3fcc8a038dc5f1941
Tri Sutrisno <mas.trisutrisno@gmail.com>
2021-06-02 14:11:03 +0700

    fix remove cart item

--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,18 +1,39 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCardButton = (props) => {
+  const [highlighted, setHighlighted] = useState(false);
+
   const cartCtx = useContext(CartContext);
 
   const numberOfItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const { items } = cartCtx;
+
+  const buttonClasses = `${classes.button} ${highlighted ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={buttonClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
